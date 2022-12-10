35b05e1cf45d6778df38ee2942b891bf9944ae88
NhungTranPY <nhungtranpy91@gmail.com>
2022-12-10 20:46:16 +0700

    174. Fetching and Validating User Input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,13 @@
 // const addMovieModal = document.querySelector('#add-modal')
 // const addMovieModal = document.body.children[1]
 // const backdrop = document.body.firstElementChild
+// const unserInputs1 = addMovieModal.getElementsByTagName('input')
 const addMovieModal = document.getElementById('add-modal')
 const startAddMovieButton = document.querySelector('header button')
 const backdrop = document.getElementById('backdrop')
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll('input')
 
 const toggleBackdrop= () => {
     backdrop.classList.toggle('visible')
@@ -15,8 +18,25 @@ const toggleMovieModal = () => {
     toggleBackdrop()
 }
 
-const cancelAddButton = () => {
+const cancelAddButtonHandler = () => {
     toggleMovieModal()
+}
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value
+    const imageURLInput = userInputs[1].value
+    const ratingValue = userInputs[2].value
+
+    if (
+        titleValue.trim() === '' || 
+        imageURLInput.trim() === '' || 
+        ratingValue.trim() === '' ||
+        +ratingValue < 1 || // + to covert string number to number
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid values (rating between 1 and 5)')
+        return
+    }
 }
 
 const backdropClickHandler = () => {
@@ -25,4 +45,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', backdropClickHandler)
-cancelAddMovieButton.addEventListener('click', cancelAddButton)
+cancelAddMovieButton.addEventListener('click', cancelAddButtonHandler)
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
